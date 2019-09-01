1f3144dec952e6e46b17cde560c6696a8c221409
benzou765 <benchan013@hotmail.com>
2019-09-01 20:43:11 +0900

    一応、完成

--- src/lib.rs
+++ src/lib.rs
@@ -346,21 +346,9 @@ unsafe fn create_block() {
         6 => NEXT_BLOCK = I_BLOCK,
         _ => NEXT_BLOCK = T_BLOCK,
     }
-    let score = (SCORE / 100) % 10;
-    match score {
-        0 => FALL_TIME = 60,
-        1 => FALL_TIME = 54,
-        2 => FALL_TIME = 48,
-        3 => FALL_TIME = 42,
-        4 => FALL_TIME = 36,
-        5 => FALL_TIME = 30,
-        6 => FALL_TIME = 24,
-        7 => FALL_TIME = 18,
-        8 => FALL_TIME = 12,
-        9 => FALL_TIME =  6,
-        _ => {}
-    }
-    // 座標の初期化
+    let level = (SCORE / 100) % 10; // 100点取得するたびにレベルを変更。10段階まで
+    FALL_TIME = 60 - (6 * level); // 1レベル変化するたびに0.1秒短縮
+                                  // 座標の初期化
     X = 5;
     Y = 0;
 }
